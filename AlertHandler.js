0ab5a64997e73f5f3346011b463cf40cc0b3ccb0
Tanya P <nobody@donotreply.org>
2015-09-08 13:10:00 -0700

    final changes

--- AlertHandler.js
+++ AlertHandler.js
@@ -5,15 +5,6 @@ AlertHandler = function(db) {
   this.db = db;
 };
 
-//var AlertHandler = function() {
-//	this.createAlert = handleCreateAlert;
-//	this.getAlerts = handleGetAlerts;
-//	this.getActiveAlerts = handleGetActiveAlerts;
-//	this.getMaintenanceAlerts = handleGetMaintenanceAlerts;
-//	this.updateAlerts = handleUpdateAlerts;
-//	this.createAudit = handleCreateAudit;
-//};
-
 AlertHandler.prototype.findAll = function(collectionName, callback) {
     this.getCollection(collectionName, function(error, the_collection) { 
       if( error ) callback(error);
@@ -76,82 +67,5 @@ AlertHandler.prototype.get = function(collectionName, id, callback) {
     });
 };
 
-AlertHandler.prototype.handleGetAlerts = function(collectionName, callback) {
-    var collection = db.collection('alerts', function(error, the_collection) {
-      if( error ) callback(error);
-      else {
-        collection.find().toArray(function(error, results) {
-          if( error ) callback(error);
-          else callback(null, results);
-        });
-      }
-    });
-};
-
-AlertHandler.prototype.handleCreateAlert = function(collectionName, obj, callback) {
-    this.getCollection('alerts', function(error, the_collection) {
-      if( error ) callback(error)
-      else {
-        obj.created_at = new Date();
-        the_collection.insert(obj, function() {
-          callback(null, obj);
-        });
-      }
-    });
-};
-
-
-AlertHandler.prototype.handleGetActiveAlerts = function(collectionName, callback) {
-   db.collection('alerts', function(error, the_collection) {
-      if( error ) callback(error);
-      else {
-        the_collection.find({ "event": "Active" }).toArray(function(error, results) {
-          if( error ) callback(error);
-          else callback(null, results);
-        });
-      }
-    });
-};
-
-AlertHandler.prototype.handleGetMaintenanceAlerts = function(collectionName, callback) {
-    this.getCollection('alerts', function(error, the_collection) { 
-      if( error ) callback(error);
-      else {
-        the_collection.find({ "event": "Scheduled Maintenance" } ).toArray(function(error, results) { 
-          if( error ) callback(error);
-          else callback(null, results);
-        });
-      }
-    });
-};
-
-AlertHandler.prototype.handleUpdateAlerts = function(collectionName, obj, entityId, callback) {
-    this.getCollection(collectionName, function(error, the_collection) {
-        if (error) callback(error);
-        else {
-            obj._id = ObjectID(entityId);
-            obj.updated_at = new Date();
-            the_collection.save(obj, function(error,doc) {
-                if (error) callback(error);
-                else callback(null, obj);
-            });
-        }
-    });
-};
-
-
-AlertHandler.prototype.handleCreateAudit = function(collectionName, obj, entityId, callback) {
-    this.getCollection(collectionName, function(error, the_collection) {
-        if (error) callback(error);
-        else {
-            obj._id = ObjectID(entityId);
-            obj.updated_at = new Date();
-            the_collection.save(obj, function(error,doc) {
-                if (error) callback(error);
-                else callback(null, obj);
-            });
-        }
-    });
-};
 
 module.exports = AlertHandler;
--- index.js
+++ index.js
@@ -7,15 +7,15 @@ var methodOverride = require('method-override');
 var errorhandler = require('errorhandler');
 
 var AlertHandler = require('./AlertHandler');
-//var routes = require('./Routes/alerts');
 var http = require('http');
 var path = require('path');
-var server = require('mongodb').Server;
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var router = express.Router();
 //var config = require('./Config');
 var winston = require('winston');
 
+var nodemailer = require("nodemailer");
 
 //---------------------------//
 //Setup Logging
@@ -46,12 +46,14 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//app.use(errorhandler({ dumpExceptions: true, showStack: true }));
+app.use(errorhandler({ dumpExceptions: true, showStack: true }));
 
 //---------------------------//
 //Setup Mongo
 //---------------------------//
 var MongoClient = require('mongodb').MongoClient;
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
 var assert = require('assert')
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
@@ -80,14 +82,14 @@ app.get('/:collection', function(req, res) {
         });
 });
 
-//app.post('/:collection', function(req, res) { 
-//    var object = req.body;
-//    var collection = req.params.collection;
-//    alertHandler.save(collection, object, function(err,docs) {
-//          if (err) { res.send(400, err); }
-//          else { res.send(201, docs); } 
-//     });
-//});
+app.post('/:collection', function(req, res) { 
+    var object = req.body;
+    var collection = req.params.collection;
+    alertHandler.save(collection, object, function(err,docs) {
+          if (err) { res.send(400, err); }
+          else { res.send(201, docs); } 
+     });
+});
 
 
 app.put('/:collection/:entity', function(req, res) { 
@@ -119,83 +121,92 @@ app.get('/:collection/:entity', function(req, res) {
    }
 });
 
+//Send an email
+app.post('/mail',function(req, res) {
+   sendEmail(req.body, function(err)  {
+   if (err)
+       res.send(err);
 
-//Set the REST API calls
-//GET AllAlerts
-app.get('/AllAlerts', function(req, res) { 
-   var params = req.params; 
-   alertHandler.handleGetAlerts(req.params.collection, function(error, objs) { 
-          if (error) { res.send(400, error); } 
-              else {
-                  if (req.accepts('html')) { 
-                  res.render('data',{objects: objs, collection: req.params.collection}); 
-              } else {
-                  res.set('Content-Type','application/json'); 
-                  res.send(200, objs); 
-              }
-         }
-        });
+   res.json({ message: 'Email Sent' });
+   });
+
+});
+
+function sendEmail(mailDetails){
+  var nodemailer = require('nodemailer');
+
+  // create reusable transporter object using SMTP transport
+  var transporter = nodemailer.createTransport({
+     service: 'mail',
+     auth: {
+        user: '',//put in your credentials
+        pass: ''
+    }
+  });
+
+
+ // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: mailDetails.from,        // sender address
+      to: mailDetails.to,            // list of receivers
+      subject: mailDetails.subject,  // Subject line
+      text: mailDetails.body         // plaintext body
+  };
+
+ // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }  else {
+          console.log('Message sent: ' + info.response)
+      };
+ });
+
+}
+
+//---------------------------//
+//Tail the alerts.json file
+//write the changes to mongo
+//---------------------------//
+//Tail the alerts-json file and create the new alerts
+var Tail = require('node.tail');
+var fs = require('fs');
+
+var fileToTail = "alerts.json";
+var lineSeparator= "]}},";
+var sleepDuration = 1000;
+
+var tail = new Tail(fileToTail,{sep: lineSeparator, follow: true, sleep: sleepDuration })
+
+tail.on('line', function(data) {
+  console.log("got new JSON:", data );
+
+  //Save the Data as an Alert if file is not truncated
+  if (data.indexOf('was cut') > -1 ){
+      console.log("Data was cut from the file:", data );
+  }
+  else
+  {
+    console.log("trying to save JSON:", data );
+    // TODO - Get the alerts to save
+    //alertHandler.save('alerts', alert, function(err,docs) {
+    //    if (err) { console.log('error:' + err); }
+    //    else { console.log('Alert created!' ); }
+    // });
+  }
+
+});
+
+tail.on('error', function(data) {
+  console.log("error:", data);
 });
 
 
-//GET ActiveAlerts
-app.get('/ActiveAlerts', function(req, res) {
-   var params = req.params;
-   alertHandler.handleGetActiveAlerts(req.params.collection, function(error, objs) {
-          if (error) { res.send(400, error); }
-              else {
-                  if (req.accepts('html')) {
-                  res.render('data',{objects: objs, collection: req.params.collection});
-              } else {
-                  res.set('Content-Type','application/json');
-                  res.send(200, objs);
-              }
-         }
-        });
-});
+//---------------------------//
+// Start the Server
+//---------------------------//
 
-//GET MaintenanceAlerts
-app.get('/MaintenanceAlerts', function(req, res) {
-   var params = req.params;
-   alertHandler.handleGetMaintenanceAlerts(req.params.collection, function(error, objs) {
-          if (error) { res.send(400, error); }
-              else {
-                  if (req.accepts('html')) {
-                  res.render('data',{objects: objs, collection: req.params.collection});
-              } else {
-                  res.set('Content-Type','application/json');
-                  res.send(200, objs);
-              }
-         }
-        });
-});
-
-//POST CreateAlert
-app.post('/Alerts', function(req, res) {
-    var object = req.body;
-    var collection = req.params.collection;
-    alertHandler.handleCreateAlert(collection, object, function(err,docs) {
-          if (err) { res.send(400, err); }
-          else { res.send(201, docs); }
-     });
-});
-
-//PUT UpdateAlert
-app.put('/Alerts/Alert', function(req, res) {
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       alertHandler.update(collection, req.body, entity, function(error, objs) { 
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); }
-       });
-   } else {
-       var error = { "message" : "Cannot PUT a whole collection" };
-       res.send(400, error);
-   }
-});
-
+//Start the server
 console.log("Logger started.");
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -210,22 +221,4 @@ http.createServer(app).listen(app.get('port'), function(){
 
 
 console.log("Successfully started web server. Waiting for incoming connections...");
-
-
-//function start() {
-//  console.log("Starting express");
-
-  //routes.setup(app, handlers);
-  //var port = process.env.PORT || 3000;
-  //app.listen(port);
-
-// http.createServer(app).listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-//  });
-
-//}
-
-// *******************************************************
-//exports.start = start;
-//exports.app = app;
 
