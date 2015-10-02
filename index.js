a10949b0d679fb3098ef4b32ef10426bcbd8108a
Steve Kallestad <stevenk1@stanford.edu>
2015-10-02 16:27:45 -0700

    default port

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
 app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'));
 
-app.set('port', process.env.PORT || 8443);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
